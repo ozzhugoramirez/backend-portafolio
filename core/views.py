1428dc7da54657e96f532492ab3d8d85c69de264
ozzhugoramirez <mycorreohugo@gmail.com>
2026-09-11 23:08:50 -0300

    primera carga de datos enel templates

--- core/views.py
+++ core/views.py
@@ -1,13 +1,18 @@
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import render
 from django.views import View
-
+from api.models import Profile, Project
 
 class HomeView(View):
     def get(self, request):
+        
+        profile = Profile.objects.first()
+        
+       
+        projects = Project.objects.filter(is_public=True)
 
         context = {
-           
+            'profile': profile,
+            'projects': projects,
         }
 
         return render(request, "pages/index.html", context)
